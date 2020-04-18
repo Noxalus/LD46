95329e3b668e19221b3436c2caca0357778f34eb
Noxalus <noxalus@gmail.com>
2020-04-18 11:28:24 +0200

    Add a ghost item to show where we want to place the item on the map.

--- LD46/Assets/Scripts/ItemPlacer.cs
+++ LD46/Assets/Scripts/ItemPlacer.cs
@@ -10,9 +10,19 @@ public class ItemPlacer : MonoBehaviour
     private NavMeshSurface _navMesh = null;
 
     [SerializeField]
-    private LayerMask _groundMask = 0;
+    private Material _ghostMaterial = null;
+
+    [SerializeField]
+    private Material _ghostMaterialDisabled = null;
+
+    [SerializeField]
+    private LayerMask _groundLayer = 0;
+
+    [SerializeField]
+    private LayerMask _ghostLayer = 0;
 
     private GameObject _currentItem = null;
+    private GameObject _ghostItem = null;
 
     void Start()
     {
@@ -24,20 +34,69 @@ public class ItemPlacer : MonoBehaviour
         if (_currentItem != null)
         {
             Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            bool isOnGround = false;
 
-            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _groundMask, QueryTriggerInteraction.Ignore))
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _groundLayer, QueryTriggerInteraction.Ignore))
             {
-                if (Input.GetKeyDown(KeyCode.Mouse0))
-                {
-                    Instantiate(_currentItem, hit.point, Quaternion.identity);
-                    _navMesh.BuildNavMesh();
-                }
+                _ghostItem.transform.position = hit.point;
+                isOnGround = true;
+            }
+
+            _ghostItem.SetActive(isOnGround);
+
+            if (isOnGround && Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Instantiate(_currentItem, _ghostItem.transform.position, Quaternion.identity);
+                _navMesh.BuildNavMesh();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                SetItem(null);
             }
         }
     }
 
     public void SetItem(GameObject item)
     {
+        if (_ghostItem != null)
+        {
+            Destroy(_ghostItem);
+        }
+
         _currentItem = item;
+
+        if (item != null)
+        {
+            CreateGhost(item);
+        }
+    }
+
+    private void CreateGhost(GameObject item)
+    {
+        _ghostItem = Instantiate(item);
+        _ghostItem.name = "Ghost";
+        _ghostItem.layer = _ghostLayer;
+
+        // Set ghost material on all meshes
+        MeshRenderer[] meshRenderers = _ghostItem.GetComponentsInChildren<MeshRenderer>();
+
+        foreach (var meshRenderer in meshRenderers)
+        {
+            meshRenderer.material = _ghostMaterial;
+        }
+
+        //// Remove all colliders (not triggers)
+        //Collider[] colliders = _ghostItem.GetComponentsInChildren<Collider>();
+
+        //foreach (var collider in colliders)
+        //{
+        //    if (!collider.isTrigger)
+        //    {
+        //        Destroy(collider);
+        //    }
+        //}
+
+        _ghostItem.SetActive(false);
     }
 }
