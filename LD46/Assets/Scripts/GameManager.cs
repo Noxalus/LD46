ebb724c3632dbf72db537a6f7450b8b945774473
Noxalus <noxalus@gmail.com>
2020-04-18 10:34:14 +0200

    Move the king using its nav mesh agent and test if it can reach the target.

---
+++ LD46/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField]
+    private Camera _camera = null;
+
+
+    [SerializeField]
+    private NavMeshAgent _king = null;
+
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+            {
+                NavMeshPath path = new NavMeshPath();
+                _king.CalculatePath(hit.point, path);
+                if (path.status == NavMeshPathStatus.PathPartial)
+                {
+                    Debug.Log("Can't reach the destination");
+                }
+
+                _king.SetDestination(hit.point);
+            }
+        }
+    }
+}
